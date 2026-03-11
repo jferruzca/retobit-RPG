01c669041ddf9d7483452a769788ecc2bef46832
Jesus Ferruzca <ferruxca@gmail.com>
2026-03-11 12:54:24 -0600

    RetoBit de Seccion 12 realizado, JFA

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,14 +1,43 @@
-class Character implements Combatant {
+public abstract class Character implements Combatant {
     // 🗒️ PROPIEDADES
     protected String name;
     protected int health;
-    // le falta el status
+    protected Status status;
 
     // 🏗️ CONSTRUCTOR
-    protected Character(String name, int health) {
+    public Character(String name, int health) {
         this.name = name;
         this.health = health;
-        // el status inicial debe ser siempre REGULAR
+        this.status = Status.REGULAR;
     }
 
+    @Override
+    public void receiveDamage(int damage) {
+        if(damage > this.health){
+            this.health = 0;
+        }else{
+            this.health -= damage;
+        }
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    @Override
+    public int getCurrentHealth() {
+        return this.health;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getStatus() {
+        if(this.health<=0) this.status = Status.DEAD;
+        return String.valueOf(this.status);
+    }
 }
